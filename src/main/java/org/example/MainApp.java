2a42bc9d5121433c543e1da9c61660c92750cba1
Mateusz <mateusz.paduch@pp-si.pl>
2019-03-29 09:08:05 +0100

    first commit

---
+++ src/main/java/org/example/MainApp.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class MainApp {
+    public static void main(String[] args) throws Exception {
+
+        //Dane wejściowe
+        double rabat = 100;
+
+        List<Produkt> productsData = new ArrayList<>();
+        productsData.add(new Produkt("blender", 500d));
+        productsData.add(new Produkt("telewizor", 1500d));
+        productsData.add(new Produkt("lodowka", 500d));
+        productsData.add(new Produkt("telefon", 500d));
+        productsData.add(new Produkt("pralka", 500d));
+
+        //sprawdzam poprawnosc dostarczonych danych:
+        //lista moze miec maxymalnie 5 produktow
+        //kazdy element musi miec nazwe oraz cene wieksza niz 0
+        if(Produkt.isValidate(productsData)){
+            throw new Exception("Dane wejsciowe sa nieprawidlowe");
+        }
+
+        List<Produkt> products = Produkt.obliczWartoscRabatu(productsData,rabat);
+
+        double suma=products.stream()
+                .map(Produkt::getWartoscRabatu)
+                .reduce((s1,s2)->s1+s2).get();
+
+        if(suma!=rabat){
+            double roznica = Produkt.ustawSkale(rabat-suma);
+            products.get(products.size()-1).setWartoscRabatu(products.get(products.size()-1).getWartoscRabatu()+roznica);
+        }
+
+        products.forEach(elem-> System.out.println(String.format("%s=%2.2fzl",elem.getName(),elem.getWartoscRabatu())));
+    }
+}
+
+
+
---
+++ src/main/java/org/example/Produkt.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Produkt {
+
+    private String name;
+    private Double cena;
+    private Double przelicznikRabatu;
+    private Double wartoscRabatu;
+
+    public Double getWartoscRabatu() {
+        return wartoscRabatu;
+    }
+
+    public void setWartoscRabatu(Double wartoscRabatu) {
+        this.wartoscRabatu = ustawSkale(wartoscRabatu);
+    }
+
+    public Produkt(String name, Double cena) {
+        this.name = name;
+        this.cena = cena;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCena() {
+        return cena;
+    }
+
+    public void setCena(Double cena) {
+        this.cena = cena;
+    }
+
+    public Double getPrzelicznikRabatu() {
+        return przelicznikRabatu;
+    }
+
+    public void setPrzelicznikRabatu(Double przelicznikRabatu) {
+        this.przelicznikRabatu = przelicznikRabatu;
+    }
+
+    public static Double kosztProduktow(List<Produkt> produkty){
+        return produkty.stream()
+                .map(Produkt::getCena)
+                .reduce((s1,s2)->s1+s2).orElse(0d);
+    }
+
+    public static List<Produkt> obliczWartoscRabatu(List<Produkt> productsData, double rabat){
+        Double kosztWszystkichProduktow = Produkt.kosztProduktow(productsData);
+        return productsData.stream()
+                .map(elem ->
+                        {
+                            elem.setPrzelicznikRabatu(elem.getCena()/kosztWszystkichProduktow);
+                            elem.setWartoscRabatu(elem.getPrzelicznikRabatu() * rabat);
+                            return elem;
+                        }
+                )
+                .collect(Collectors.toList());
+    }
+
+    public static Double ustawSkale(Double d){
+        return new BigDecimal(d).setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    public static boolean isValidate(List<Produkt> produkty){
+        return produkty.size() > 5 || produkty.stream().anyMatch(elem-> elem.getCena() <= 0 || elem.getName().isEmpty());
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt{" +
+                "name='" + name + '\'' +
+                ", cena=" + cena +
+                ", przelicznikRabatu=" + przelicznikRabatu +
+                ", wartoscRabatu=" + wartoscRabatu +
+                '}';
+    }
+}
